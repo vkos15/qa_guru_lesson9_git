fdba626555cde223a2b4a96d122a03d0b72c7ef7
Valentina <15valentinka@gmail.com>
2021-10-03 04:17:25 +0500

    add second test

---
+++ src/test/java/ru/vkost/SecondTest.java
@@ -0,0 +1,13 @@
+package ru.vkost;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+
+    @Test
+    void secondTest() {
+
+        Assertions.assertTrue(true);
+    }
+}
